1eef329e3391ab50fe234fd4368ba7c1f4af8c41
yomad96 <yohan.madeira@gmail.com>
2026-02-24 19:03:11 +0100

    Update navigation labels and replace decorations list with table layout

--- app/src/App.vue
+++ app/src/App.vue
@@ -7,9 +7,9 @@
           mode="horizontal"
           :style="{ lineHeight: '64px' }"
       >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
+        <a-menu-item key="1">Decorations</a-menu-item>
+        <a-menu-item key="2">Charms</a-menu-item>
+        <a-menu-item key="3">Skills</a-menu-item>
       </a-menu>
     </a-layout-header>
     <a-layout-content :style="{ padding: '0 50px', marginTop: '64px', minHeight: 'calc(100vh - 64px)' }">
--- app/src/ui/views/ListDecorationsView.vue
+++ app/src/ui/views/ListDecorationsView.vue
@@ -1,24 +1,24 @@
 <template>
   <div>
-    <section-title title="List Decorations" />
-    <a-list item-layout="horizontal" :data-source="filteredDecorations" :loading="isLoading">
-      <template #renderItem="{ item }">
-        <a-list-item>
-          <a-list-item-meta description="Ant Design, a design language for background applications, is refined by Ant UED Team">
-            <template #title>
-              {{ item.name }}
-            </template>
-            <template #avatar>
-              <a-avatar :src="item.icon" />
-            </template>
-          </a-list-item-meta>
-        </a-list-item>
+    <section-title title="List Decorations"/>
+    <a-table :columns="columns" :dataSource="filteredDecorations" :loading="isLoading">
+      <template #bodyCell="{ column, record }">
+        <template v-if="column.key === 'skills'">
+          <div v-if="record.skills.length > 0" v-for="skill in record.skills">
+            {{ skill.name }}
+          </div>
+        </template>
+        <template v-if="column.key === 'description'">
+          <div v-if="record.skills.length > 0" v-for="skill in record.skills">
+            {{ skill.description }}
+          </div>
+        </template>
       </template>
-    </a-list>
+    </a-table>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import SectionTitle from "@/ui/components/SectionTitle.vue";
 import {computed, onMounted} from "vue";
 import {useListDecorationView} from "@/ui/composables/useListDecorationView.ts";
@@ -32,6 +32,28 @@ const {
 const isLoading = computed(() => {
   return getListDecorationStatus.value === 'loading'
 })
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Skills',
+    key: 'skills',
+  },
+  {
+    title: 'Kind',
+    dataIndex: 'kind',
+    key: 'kind',
+  },
+  {
+    title: 'Description',
+    dataIndex: ['skill', 'description'],
+    key: 'description',
+  },
+]
 
 onMounted(() => {
   getDecorations()
